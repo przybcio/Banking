a0e3a68d05593968871279048270f50705f953ae
LWP <lukasz.przybyl@bms.com.pl>
2014-01-10 15:40:03 +0100

    dopisanie komentarza

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -16,6 +16,8 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+			
+			//test form
         }
     }
 }
