b60073729352d0b9bcd53d7cc86f2f8f40756ec2
LWP <lukasz.przybyl@bms.com.pl>
2014-01-10 15:32:03 +0100

    Test1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraSplashScreen.SplashScreenManager splashScreenManager1 = new DevExpress.XtraSplashScreen.SplashScreenManager(this, null, true, true);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tileControl1 = new DevExpress.XtraEditors.TileControl();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(33, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // tileControl1
+            // 
+            this.tileControl1.Location = new System.Drawing.Point(97, 172);
+            this.tileControl1.Name = "tileControl1";
+            this.tileControl1.Size = new System.Drawing.Size(75, 23);
+            this.tileControl1.TabIndex = 1;
+            this.tileControl1.Text = "tileControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 255);
+            this.Controls.Add(this.tileControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.TileControl tileControl1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClassLibrary1;
+using ClassLibrary2;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : BaseForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BaseClass1 poo = new BaseClass1();
+        }
+    }
+}
